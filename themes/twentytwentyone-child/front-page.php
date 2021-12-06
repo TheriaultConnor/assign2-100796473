f448037c0ddf3bd8c0babb3f853f3dff16dd5069
Connor Theriault <connor.theriault@dcmail.ca>
2021-12-06 13:49:40 -0500

    Added content to front-page.php

--- themes/twentytwentyone-child/front-page.php
+++ themes/twentytwentyone-child/front-page.php
@@ -12,7 +12,23 @@ get_header(); ?>
 
 <div class="content-area" id="primaru">
     <main id="main" class="site-main" role="main">
-        <p>content area</p>
+    <div class="grid-container">
+  <div class="grid-x grid-margin-x">
+    <div class="cell lage-12 text-center"><h1>The best Toronto Maple Leafs fan page</h1></div>
+    <div  class="cell lage-12 text-center">
+   <?php while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop. 
+    ?>
+  </div>
+  </div>
+</div>
     </main>
 </div>
 
